0f5194f09899afcfbbeb6b3f778695c97acfbede
Bala <balasivarathri@gmail.com>
2021-02-14 21:47:55 +0200

    Select checkbox from data table

---
+++ src/com/selenium/tests/CheckBoxDataTable.java
@@ -0,0 +1,40 @@
+package com.selenium.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class CheckBoxDataTable {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\eclipse-workspace\\SeleniumTutorials\\Browser_LIB\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://google.com/");
+		driver.navigate().to("https://ui.cogmento.com");
+		//Thread.sleep(9000);
+		
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys("balukrishna5588@gmail.com");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("9989*Bala");
+		driver.findElement(By.xpath("//div[contains(text(),'Login')]")).click();
+	
+		
+		
+		Actions action = new Actions(driver);
+		action.moveToElement(driver.findElement(By.xpath("//div[@id='main-nav']"))).build().perform();
+		
+		driver.findElement(By.xpath("//span[contains(text(),'Contacts')]")).click();
+		action.moveToElement(driver.findElement(By.xpath("//td[contains(text(),'Lead')]"))).build().perform();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//a[contains(text(),'bala krishna sivarathri')]/parent::td//preceding-sibling::td//input[@name='id']")).click();
+		
+		}
+
+}
--- src/com/selenium/tests/JavaScriptExecutor.java
+++ src/com/selenium/tests/JavaScriptExecutor.java
@@ -16,58 +16,68 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class JavaScriptExecutor {
 
 	WebDriver driver;
+
 	public static void main(String[] args) throws IOException {
 
-
 		// To Initialize browser
-				System.setProperty("webdriver.chrome.driver",
-						"C:\\Users\\User\\eclipse-workspace\\SeleniumTutorials\\Browser_LIB\\chromedriver.exe");
-				WebDriver driver = new ChromeDriver();
-				// To maximize the window
-				driver.manage().window().maximize();
-				// To delete all cookies
-				driver.manage().deleteAllCookies();
-				// To wait the page to load
-				driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-				driver.get("https://www.facebook.com");
-				driver.findElement(By.id("email")).sendKeys("balasivarathri@gmail.com");
-				driver.findElement(By.id("pass")).sendKeys("9959855206");
-				
-				//execute Script ---- to execute java script code
-				WebElement lgnbtn= driver.findElement(By.xpath("//button[contains(text(),'Log In')]"));
-				//To highlight the element
-				flash(lgnbtn, driver);
-				
-				// To highlight the border with red color
-				drawBorder(lgnbtn, driver);
-								
-				//To Capture Screenshot and store as a file format
-				File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-				//To Store in desired location using copy file method
-				FileUtils.copyFile(src, new File("C:\\Users\\User\\Desktop\\Screenshots\\FB.jpg"));
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\User\\eclipse-workspace\\SeleniumTutorials\\Browser_LIB\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		// To maximize the window
+		driver.manage().window().maximize();
+		// To delete all cookies
+		driver.manage().deleteAllCookies();
+		// To wait the page to load
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("balasivarathri@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("995985526");
+
+		// execute Script ---- to execute java script code
+		WebElement lgnbtn = driver.findElement(By.xpath("//button[contains(text(),'Log In')]"));
+		// To highlight the element
+		//flash(lgnbtn, driver);
+
+		// To highlight the border with red color
+		//drawBorder(lgnbtn, driver);
+
+		// To Capture Screenshot and store as a file format
+		//File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		// To Store in desired location using copy file method
+		//FileUtils.copyFile(src, new File("C:\\Users\\User\\Desktop\\Screenshots\\FB.jpg"));
+		
+		//To click on element by using JavaScriptExecutor
+		clickElement(lgnbtn, driver);
 	}
 
 	public static void flash(WebElement element, WebDriver driver) {
-		JavascriptExecutor js = ((JavascriptExecutor)driver);
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
 		String bgcolour = element.getCssValue("backgroundColor");
-		for(int i=0; i<50;i++) {
-			changeColor("rgb(0,200,0)",element,driver);
-			changeColor(bgcolour,element,driver);
+		for (int i = 0; i < 10; i++) {
+			changeColor("rgb(0,200,0)", element, driver);
+			changeColor(bgcolour, element, driver);
 		}
 	}
+
 	public static void changeColor(String color, WebElement element, WebDriver driver) {
-		
-		JavascriptExecutor js = ((JavascriptExecutor)driver);
-		js.executeScript("arguments[0].style.backgroundColor='"+color+"'", element);
+
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("arguments[0].style.backgroundColor='" + color + "'", element);
 		try {
 			Thread.sleep(20);
-		}catch(InterruptedException e) {
-			
+		} catch (InterruptedException e) {
+
 		}
 	}
+
 	public static void drawBorder(WebElement element, WebDriver driver) {
-		JavascriptExecutor js = ((JavascriptExecutor)driver);
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
 		js.executeScript("arguments[0].style.border='9px solid red'", element);
+	}
+	public static void clickElement(WebElement element ,WebDriver driver) {		
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("arguments[0].click()", driver);
+		
 	}
 }
